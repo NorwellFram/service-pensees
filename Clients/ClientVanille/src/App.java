08452e1632d946f21f52f4a74bc6b78a0e4d8e2f
NorwellFram <46940569+NorwellFram@users.noreply.github.com>
2019-02-21 15:20:19 -0500

    ajout du cache

--- Clients/ClientVanille/src/App.java
+++ Clients/ClientVanille/src/App.java
@@ -1,5 +1,12 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Iterator;
 import java.util.List;
+
+import Vue.VuePensee;
 import accesseur.PenseeDAO;
 import modele.Pensee;
 import outils.Journal;
@@ -8,18 +15,33 @@ import Vue.VuePensee;
 public class App {
 
 	public static void main(String[] parametres) {
+
+		String DSN = "jdbc:sqlite:C:/sqlite/bases/inspiration.cache.db";
+		String SQL_ENREGISTRER_PENSEE = "insert into pensee(auteur, message) values('','')";
+		String SQL_LISTER_PENSEES = "SELECT * FROM pensee";
+		
+		Connection basededonnees = null;
+		try {
+			basededonnees = DriverManager.getConnection(DSN);
+			Statement requeteListePensees = basededonnees.createStatement();
+			ResultSet curseurListePensees = requeteListePensees.executeQuery(SQL_LISTER_PENSEES);
+			curseurListePensees.next();
+			String auteur = curseurListePensees.getString("auteur");
+			String pensee = curseurListePensees.getString("message");
+			System.out.println(auteur + " - " + pensee);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		
 		//Journal.activer();
-				Journal.activerNiveau(0);
-				
-
-		VuePensee.launch(VuePensee.class, parametres);
-
+		Journal.activerNiveau(0);
+		
 		//Pensee pensee = new Pensee("Rossetti","Ce qui est plus triste qu une oeuvre inachevee, c est une oeuvre jamais commencee.");
-		//penseeDAO.ajouterPensee(pensee);
-
+		//penseeDAO.ajouterPensee(pensee);		
+		
+		VuePensee.launch(VuePensee.class, parametres);
+		
+		
 	}
-	
 
 }
-
